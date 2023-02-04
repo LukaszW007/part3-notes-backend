80f27b22f8fe3eccf8e41e7c8f646cfcb6ba323e
LukaszW007 <https://LukaszW007@github.com>
2023-02-04 21:48:44 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 require('dotenv').config()
 const express = require('express')
-const Note = require('./models/note')
-
 const app = express()
+const cors = require('cors')
 
-const url = `mongodb+srv://luwi:${password}@todolist.gdm9f13.mongodb.net/noteApp?retryWrites=true&w=majority`
+app.use(cors())
+const mongoose = require('mongoose')
+
+// const Note = require('./models/note')
+
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
+
+console.log('connecting to', url)
+
 mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const noteSchema = new mongoose.Schema({
     content: String,
@@ -24,26 +37,26 @@ noteSchema.set('toJSON', {
 
 const Note = mongoose.model('Note', noteSchema)
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2022-01-10T17:30:31.098Z",
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2022-01-10T18:39:34.091Z",
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2022-01-10T19:20:14.298Z",
+//     important: true
+//   }
+// ]
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -68,16 +81,14 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/api/notes', (req, res) => {
@@ -94,15 +105,10 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
+}) ///do sprawdzenia lokalnie, pewnie zmiany w put, delete i get po id
 
 app.put('/api/notes/:id', (request, response)=> {
     const id = request.params.id;
@@ -121,7 +127,7 @@ app.put('/api/notes/:id', (request, response)=> {
 })
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
