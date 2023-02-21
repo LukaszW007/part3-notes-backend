b489048fca6daaa4341b0cf54a7f0c298b9876e7
LukaszW007 <https://LukaszW007@github.com>
2023-02-21 18:24:30 +0100

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ require('dotenv').config()
 app.use(cors())
 const Note = require('./models/note')
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
 
 // let notes = [
 //   {
@@ -28,8 +36,9 @@ const Note = require('./models/note')
 //   }
 // ]
 
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -42,55 +51,118 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post('/api/notes', (request, response, next) => {
+    const body = request.body
 
-  if (!body.content) {
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+    })
+
+  // if (!body.content) {
+  //   return response.status(400).json({
+  //     error: 'content missing'
+  //   })
+  // }
+  if (!body.important) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'important missing'
     })
   }
 
-  Note.create(body).then(savedNote => {
-        response.json(savedNote)
-    })
+  Note.create(body)
+      .then(savedNote => {
+          response.json(savedNote.toJSON())
+      })
+      .catch(error => next(error))
 })
 
 app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes=> {
-      res.json(notes);
-  })
+  Note.find({})
+      .then(notes=> {
+          res.json(notes);
+      })
+      .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findByIdAndRemove(request.params.id).then(deletedCount => {
-        if (deletedCount) {
-            console.log("item with id ",request.params.id," is deleted")
-        } else {
-            console.log("item with id ",request.params.id," not found")
-        }
-        response.status(204).end()
-    })
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(deletedCount => {
+            if (deletedCount) {
+                console.log("item with id ",request.params.id," is deleted")
+            } else {
+                console.log("item with id ",request.params.id," not found")
+            }
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.put('/api/notes/:id', (request, response)=> {
-    Note.findByIdAndUpdate(request.params.id, request.body).then(note => {
-        response.json(note)
-        console.log("item updated to ",request.body)
-    })
+
+app.put('/api/notes/:id', (request, response, next)=> {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+    Note.findByIdAndUpdate(request.params.id, note)
+        .then(updatedNote => {
+            response.json(updatedNote)
+            console.log("item updated to ",request.body)
+        })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    // console.error('ERROR',error)
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400)
+            .send({ type: error.type,
+                    error: error.message
+            })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400)
+            .send({ type: error.type,
+                    error: error.message
+            })
+    } else if (error.name === 'SyntaxError') {
+        return response.status(400)
+            .send({ type: error.type,
+                    error: error.message
+            })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
+
+////////////////////////////////////////////
 //problems with deploy:full
 // --> Verified app config
 // ==> Building image
